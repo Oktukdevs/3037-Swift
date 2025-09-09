d297a2c17255c1017b5d3f9d33b0427e1c79570b
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-09 10:59:01 +0300

    init

---
+++ BallsConnect/App/BallsConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallsConnectApp.swift
+//  BallsConnect
+//
+//  Created by Serhii Babchuk on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BallsConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BCLaunchView()
+        }
+    }
+}
